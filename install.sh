5c5e85e338f3a1d65c3750edf8e65fc6b20cb3fe
taian_l <taianhacks.il@gmail.com>
2026-03-21 18:51:10 -0500

    feat: install script

---
+++ install.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Install packages
+install_pkgs() {
+  # Official
+  local OFFICIAL=.official_pkgs.txt
+  local NF_PKGS=$(wc -l <$OFFICIAL)
+
+  # AUR
+  local AUR=.aur_pkgs.txt
+  while read -r pkg; do
+    sudo pacman -S "$pkg" --noconfirm --needed
+  done <"$OFFICIAL"
+
+  echo -e "[+] Official packages installed"
+
+  while read -r pkg; do
+    yay -S "$pkg" --noconfirm --needed --answerdiff None --answerclean All
+  done <"$AUR"
+
+  echo "[+] AUR packages installed"
+}
+
+dotfiles_install() {
+  echo "[+] Starting the dotfile linking process using GNU stow"
+  for dir in */; do
+    pkg="${dir%/}"
+
+    if [ "$pkg" == ".git" ]; then
+      continue
+    fi
+
+    echo "[+] --> Linking package $pkg"
+    stow "$pkg"
+  done
+  echo "[+]Configuration applied successfully"
+}
+
+# Spiner
+spiner() {
+  local PID=$1
+  local DELAY=0.1
+  local SPIN='|/-\'
+  echo -n "[+] Wait :) "
+  tput civis
+  while [ "$(ps a | awk '{print $1}' | grep $PID)" ]; do
+    local temp=${SPIN#?}
+    printf " [%c]  " "$SPIN"
+    local SPIN=$temp${SPIN%"$temp"}
+    sleep $DELAY
+    printf "\b\b\b\b\b\b"
+    printf "    \b\b\b\b"
+  done
+  tput cnorm
+}
+
+# Clean cursor
+clean_cursor() {
+  tput cnorm
+  echo -e "\n "
+  kill "$INSTALL_PID" >/dev/null
+  echo "[+] Exit signal detected..."
+  exit
+}
+
+# MAIN FUNCTION
+main() {
+  echo "[+] To continue with the installation, root permissions are required."
+  sudo -v
+  echo -e " "
+  if ! uname -r | grep "cachy" >/dev/null; then
+    echo -e "\n[+] This script is designed for CachyOS, it's not recommend use in other distros because is not completly tested. Conflicts with some critical system packages are likely."
+    exit 1
+  else
+    echo "[+] Correct distro detected: CachyOS"
+  fi
+
+  echo "[+] The necessary packages will be installed from both the official repository and AUR. If the AUR is not installed, it will be installed..."
+
+  if ! which "yay" >/dev/null; then
+    sudo pacman -S yay
+  fi
+
+  install_pkgs &
+  INSTALL_PID=$!
+  spiner "$INSTALL_PID"
+  dotfiles_install
+}
+
+# EXECUTION
+trap clean_cursor SIGINT EXIT
+main
